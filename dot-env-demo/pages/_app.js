8b913e656b53eadeb693ee1fb5821dad029c52af
chanseok <hi007chans@gmail.com>
2020-12-13 15:21:49 +0900

    설정: antd css 사용 설정 및 ES6 support by npm i esm

--- dot-env-demo/pages/_app.js
+++ dot-env-demo/pages/_app.js
@@ -1,4 +1,5 @@
 import '../styles/index.css'
+import 'antd/dist/antd.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
--- dot-env-demo/pages/index.js
+++ dot-env-demo/pages/index.js
@@ -1,15 +1,19 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import { Button } from 'antd'
 
 export default function Home() {
 
   return (
     <div className="flex justify-center items-center h-screen">
       <div>
-        <h1> Hello Next.js </h1>
+        <h1> Hello Next.js with antd</h1>
         <p>
           API_HOST: {process.env.API_HOST}
         </p>
+      <div>
+        <Button>Welcome</Button>
+      </div>
       </div>
     </div>
   )
